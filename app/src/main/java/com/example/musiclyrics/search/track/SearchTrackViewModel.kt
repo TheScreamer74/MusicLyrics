7d5daeba9b3cf97718636e4f669f48e0a6799b1b
Thomas <thomashuyghues@gmail.com>
2020-10-05 15:08:03 +0200

    resolving conflicts

--- app/src/main/java/com/example/musiclyrics/search/track/SearchTrackViewModel.kt
+++ app/src/main/java/com/example/musiclyrics/search/track/SearchTrackViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.musiclyrics.network.MusicXMatch
-import com.example.musiclyrics.network.properties.search.Track
 import com.example.musiclyrics.network.properties.search.TrackList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
