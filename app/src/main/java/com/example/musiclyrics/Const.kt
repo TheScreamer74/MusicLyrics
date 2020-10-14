6c073e38798c201e2c4d56c7ed97b66d8b146005
TheScreamer74 <34384878+TheScreamer74@users.noreply.github.com>
2020-10-14 09:41:55 +0200

    Delete Const.kt

--- app/src/main/java/com/example/musiclyrics/Const.kt
+++
@@ -1,6 +0,0 @@
-package com.example.musiclyrics
-
-
-const val API_KEY = "fb20e5b416b5d8f3bb484102abca1638"
-const val BASE_URL_MUSICXMATCH = "https://api.musixmatch.com/ws/1.1/"
-const val API_KEY_APPLE = "MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgQ4nLC+W4ytzNzgkOPV8cQxFEsAw7rNijYtX5UvM33IGgCgYIKoZIzj0DAQehRANCAAT7yObspKznpoBX1DkCKHCBeguLvIWx4wjqTAjhLPuQyPZBwF9N5fE9EjtyIPglI8ArHyoQdApNJmUc3fMg580T"
