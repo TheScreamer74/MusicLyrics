196a7f255e85d455729362e84404749aecb5177e
TheScreamer74 <thomashuyghues@gmail.com>
2021-10-21 13:46:06 +0100

    changed picasso lib -> coil lib (modified BindingAdapter.kt)

--- app/src/main/java/com/example/musiclyrics/BindingAdapter.kt
+++ app/src/main/java/com/example/musiclyrics/BindingAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.musiclyrics
 
+import android.util.Log
 import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.size.Scale
 import com.example.musiclyrics.network.properties.search.track.TrackList
 import com.example.musiclyrics.search.track.SearchTrackAdapter
-import com.squareup.picasso.Picasso
 
 
 @BindingAdapter("listDataTrack")
@@ -15,8 +18,13 @@ fun bindRecyclerViewTrack(recyclerView: RecyclerView, data: List<TrackList>?){
 }
 
 @BindingAdapter("image")
-fun bindImage(imgView: ImageView, imgUrl: String?) {
-    Picasso.get()
-        .load(imgUrl)
-        .into(imgView)
+fun ImageView.bindImage(image: String?) {
+    image?.let { url ->
+        val parsedImageUrl = url.toUri().buildUpon().scheme("https").build()
+        this.load("$parsedImageUrl") {
+            placeholder(R.drawable.ic_image_placeholder)
+            scale(Scale.FILL)
+            Log.d("API IMAGE", parsedImageUrl.toString())
+        }
+    }
 }
